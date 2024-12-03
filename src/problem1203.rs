f40fb9106fbc5f6adce40218226dc70344d092c6
alexander papageorge <alexander.papageorge@openlabsusa.org>
2024-12-02 19:19:08 -0500

    fmt

--- src/problem1203.rs
+++ src/problem1203.rs
@@ -4,7 +4,13 @@ use crate::utils::read_lines;
 
 pub fn problem() -> (usize, String, String) {
     let data_dir: String = env!("CARGO_MANIFEST_DIR").to_owned();
-    // let data_path: PathBuf = [data_dir, "src".to_string(), "input3".to_string()].iter().collect();
+    let data_path: PathBuf = [data_dir, "src".to_string(), "input3".to_string()].iter().collect();
+
+    if let Ok(lines) = read_lines(data_path) {
+        for line in lines {
+            if let Ok(x) = line {}
+        }
+    }
 
     return (2, format!("{}", 0), format!("{}", 0));
 }
--- src/problem1204.rs
+++ src/problem1204.rs
@@ -4,7 +4,12 @@ use crate::utils::read_lines;
 
 pub fn problem() -> (usize, String, String) {
     let data_dir: String = env!("CARGO_MANIFEST_DIR").to_owned();
-    // let data_path: PathBuf = [data_dir, "src".to_string(), "input4".to_string()].iter().collect();
+    let data_path: PathBuf = [data_dir, "src".to_string(), "input4".to_string()].iter().collect();
+    if let Ok(lines) = read_lines(data_path) {
+        for line in lines {
+            if let Ok(x) = line {}
+        }
+    }
 
     return (3, format!("{}", 0), format!("{}", 0));
 }
